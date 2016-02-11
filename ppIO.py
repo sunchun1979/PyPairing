53dee43371e1f1aebf9beef0f25d62c4bd97413e
Chun Sun <sunchun79@gmail.com>
2016-02-10 23:44:16 -0500

    todo: mms across divisions todo: handicap

--- ppIO.py
+++ ppIO.py
@@ -2,8 +2,24 @@ import csv
 import pandas
 
 def read_round(filename):
-    dframe = pandas.read_csv(filename)
-    dframe = dframe.fillna("")
+    rawframe = pandas.read_csv(filename)
+    rawframe = rawframe.fillna("")
+    dframe = []
+    begin = 0
+    handicap = 0
+    for i in range(0,len(rawframe)):
+        if (rawframe['id'][i]=='='):
+            dframe.append((handicap, rawframe.loc[begin:i-1].reset_index(drop=True)))
+            begin = i+1
+            handicap = 0
+        elif (rawframe['id'][i]=='&'):
+            dframe.append((handicap, rawframe.loc[begin:i-1].reset_index(drop=True)))
+            begin = i+1
+            handicap = 1
+    dframe.append((handicap,rawframe.loc[begin:].reset_index(drop=True)))
+    for df in dframe:
+        df[1]['id'] = df[1]['id'].astype(int)
+        df[1]['rank'] = df[1]['rank'].astype(float)
     return dframe
 
 def print_pairing(pairing, dframe):
--- ppMain.py
+++ ppMain.py
@@ -5,6 +5,7 @@ import ppGenRound
 import ppUpdateRound
 import ppGetMMS
 import random
+import sys
 
 def gen_winner(match_name):
     match_name['Result'] = pandas.Series(len(match_name), index=match_name.index)
@@ -15,19 +16,36 @@ def gen_winner(match_name):
 
 # beginning of main entry
 
-dfRound = ppIO.read_round("Test_Case1.txt")
-print dfRound
-
+#testCases = ['Test_Case1.txt', 'Test_Case2.txt', 'Test_Case3.txt']
+testCases = ['Test_Case1.txt']
 totalRound = 4
-for i in range(0,totalRound):
-    finalPairing, sortedFrame = ppGenRound.generate_new_round(dfRound, totalRound, i)
-    print finalPairing[0]
-    resTable = ppIO.print_pairing(finalPairing[0][1], dfRound)
-    result = gen_winner(resTable)
-    print result
-    dfRound = ppUpdateRound.update_round(sortedFrame, result)
-    dfRound = ppGetMMS.sort_by_mms(dfRound, False)
-    print dfRound
-    dfRound.drop(['mms'],1)
+
+for tFile in testCases:
+    dfRound = ppIO.read_round(tFile)
+    for trial in range(0,1):
+        resultFile = tFile.replace(".txt", "_Result_" + str(trial) + ".txt")
+        sys.stdout = open(resultFile, 'w')
+        for i,iband in enumerate(dfRound):
+            handicap = iband[0]
+            if (handicap==1):
+                handicapTxt = "(Handicap)"
+            else:
+                handicapTxt = ""
+            print "[Section " + str(i+1) + "]" + handicapTxt + ": Initial Position"
+            print iband[1]
+            band = iband[1].copy()
+            for i in range(0,totalRound):
+                finalPairing, sortedFrame = ppGenRound.generate_new_round(band, totalRound, i)
+                #print finalPairing[0]
+                resTable = ppIO.print_pairing(finalPairing[0][1], band)
+                result = gen_winner(resTable)
+                print "\nResult after round " + str(i+1) + ":"
+                print result
+                band = ppUpdateRound.update_round(sortedFrame, result)
+                band = ppGetMMS.sort_by_mms(band, False)
+                print "\nStanding after round " + str(i+1) + ":"
+                print band
+                band.drop(['mms'],1)
+            print "\n=======\n"
 
 
